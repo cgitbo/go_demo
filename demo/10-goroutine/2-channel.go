b065833a4115e9b8c888e074e6e3b2f52c25a041
flx <mbp13@flx.com>
2023-01-29 08:02:08 +0800

    modified:   demo/10-goroutine/2-channel.go

--- demo/10-goroutine/2-channel.go
+++ demo/10-goroutine/2-channel.go
@@ -1,14 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	// 管道通信
 	c := make(chan int)
 
 	go func() {
-		defer fmt.Println("end")
+		// 写入文件
+		c <- 666
 		fmt.Println("running")
+		defer fmt.Println("end")
 	}()
 
-	fmt.Println(c)
+	go func() {
+		fmt.Println("running2")
+		c <- 1
+		defer fmt.Println("end2")
+	}()
+
+	// 读取
+	for {
+		time.Sleep(time.Second)
+		fmt.Println(<-c)
+	}
+
 }
